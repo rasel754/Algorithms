c58aa15499b5f8c7b385bf9f6b22b1977147cc29
unknown <raselahmed73614@gmail.com>
2025-08-19 01:24:17 +0600

    dfs code

---
+++ demo.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+class Node
+{
+public:
+    int value;
+    Node *left;
+    Node *rignt;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->left = NULL;
+        this->rignt = NULL;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
